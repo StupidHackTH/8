be35087259752e864829c7d48caf5785d0c1d722
DrowningToast <supratouchgussii@gmail.com>
2024-06-16 15:48:39 +0700

    feat: agenda

---
+++ src/components/Landing/agenda.ts
@@ -0,0 +1,54 @@
+export const agenda = {
+	"13 July 2024": [
+		{
+			time: "09.00 - 09.45",
+			title: "registration",
+		},
+		{
+			time: "09.45 - 10.00",
+			title: "opening speech",
+		},
+		{
+			time: "10.00 - 10.45",
+			title: "icebreaking",
+		},
+		{
+			time: "10.45 - 11.45",
+			title: "how stupid idea are made (talk show)",
+		},
+		{
+			time: "11.45 - 13.00",
+			title: "eat eat",
+		},
+		{
+			time: "13.00 - 15.00",
+			title: "project brian storming",
+		},
+		{
+			time: "15.00 - 06.00",
+			title: "Let's Hack!!!!!",
+		},
+	],
+	"14 July 2024": [
+		{
+			time: "06.00 - 07.00",
+			title: "photosynthesis",
+		},
+		{
+			time: "07.00 - 12.00",
+			title: "hack continue",
+		},
+		{
+			time: "12.00 - 13.00",
+			title: "eat eat",
+		},
+		{
+			time: "13.00 - 13.30",
+			title: "present set up",
+		},
+		{
+			time: "13.30 - 18.00",
+			title: "pitching time!",
+		},
+	],
+};
