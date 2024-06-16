62650c6b7438c45c406d9b3ce31b04ac56aea1b9
betich <betich365@gmail.com>
2024-06-17 01:20:32 +0700

    fix: fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
   base: "/8",
   site: "https://stupid.hackathon.in.th/8",
-  adapter: cloudflare(),
 });
