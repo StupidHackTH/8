2bf0255e1f22bc68264d43dcb46c96e1c285befd
betich <betich365@gmail.com>
2024-06-16 18:08:36 +0700

    chore: add base to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	output: "hybrid",
-	adapter: cloudflare(),
+  integrations: [tailwind()],
+  output: "hybrid",
+  base: "/8",
+  site: "https://stupid.hackathon.in.th/8",
+  adapter: cloudflare(),
 });
